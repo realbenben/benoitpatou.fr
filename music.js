a67fc230c65419cfc8a2c7f4dd545dae86c197d5
realbenben <b.patou59@gmail.com>
2020-09-12 16:45:22 +0200

    update ocarina project

--- music.js
+++ music.js
@@ -1,5 +1,14 @@
 const sound = new Howl({
-  src: ['sound.mp3']
+  src: ['./sound/OOT_Notes_Ocarina_A_med.wav'],
+html5: true,
+buffer: true,
+ autoplay: true,
+  loop: true,
+  volume: 0.5,
+  onend: function() {
+    console.log('Finished!');
+  }
 });
-
+console.log(sound);
 sound.play();
+console.log("yolo");
