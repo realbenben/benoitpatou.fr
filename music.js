09558285fc374b65e105f17d0ee0edd972860dfe
David Leuliette <dleuliette@gmail.com>
2020-09-12 17:22:34 +0200

    Add mp3

--- music.js
+++ music.js
@@ -1,14 +1,15 @@
-console.log("yolo");
+console.log('yolo');
+console.log(ion);
+
 ion.sound({
   sounds: [
-    { name: 'OOT_Notes_Ocarina_A_med.wav' },
-    { name: 'OOT_Notes_Ocarina_B_med.wav' },
-    { name: 'OOT_Notes_Ocarina_D2_med.wav' },
-    { name: 'OOT_Notes_Ocarina_D_med.wav' },
-    { name: 'OOT_Notes_Ocarina_F_med.wav' },
+    { name: 'OOT_Notes_Ocarina_A_med' },
+    { name: 'OOT_Notes_Ocarina_B_med' },
+    { name: 'OOT_Notes_Ocarina_D2_med' },
+    { name: 'OOT_Notes_Ocarina_D_med' },
+    { name: 'OOT_Notes_Ocarina_F_med' },
   ],
   volume: 0.5,
   path: './sound/',
   preload: true,
 });
-console.log(ion);
